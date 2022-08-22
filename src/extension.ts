422cc31358c077d3810cf2f977d977d42ecdc423
Zarigani <zari220130@gmail.com>
2022-08-22 10:57:04 +0900

    Add highlighting feature

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,8 @@ import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
   /* Debug */
-  // console.log('"Repsel" is now active!');
-  // vscode.window.showInformationMessage('"Repsel" is now active!');
+  console.log('"Repsel" is now active!');
+  vscode.window.showInformationMessage('"Repsel" is now active!');
 
   let replaceInSelection = vscode.commands.registerCommand(
     "repsel.replaceInSelection",
@@ -27,16 +27,53 @@ export function activate(context: vscode.ExtensionContext) {
           prompt: "Target text",
           value: "",
         });
+
+        const doc = editor.document;
+        const selectedText = doc.getText(textSelectionRange);
+        const targetRegExp = new RegExp(queryTarget ? queryTarget : "", "g");
+
+        /* Highlighting - begin */
+        const decorationType = vscode.window.createTextEditorDecorationType({
+          backgroundColor: "green",
+        });
+        var decorationTargets: vscode.DecorationOptions[] = [];
+        var matches = selectedText.match(targetRegExp);
+        console.log(matches);
+
+        var selectionStartOffset = doc.offsetAt(editor.selection.start);
+
+        var searchStartPos = 0;
+        if (matches) {
+          for (let i = 0; i < matches.length; i++) {
+            let searchArea = selectedText.slice(searchStartPos);
+            let wordPos = searchArea.search(targetRegExp);
+            let word = matches[i];
+
+            let range = new vscode.Range(
+              doc.positionAt(selectionStartOffset + searchStartPos + wordPos),
+              doc.positionAt(
+                selectionStartOffset + searchStartPos + wordPos + word.length
+              )
+            );
+
+            searchStartPos += wordPos + word.length;
+
+            decorationTargets.push({
+              range,
+            });
+          }
+        }
+
+        editor.setDecorations(decorationType, decorationTargets);
+        /* Highlighting - end */
+
         const queryNew = await vscode.window.showInputBox({
           placeHolder: "",
           prompt: "New text",
           value: "",
         });
 
-        const document = editor.document;
-        const selectedText = document.getText(textSelectionRange);
-
-        const targetRegExp = new RegExp(queryTarget ? queryTarget : "", "g");
+        decorationType.dispose();
 
         const replacedText = selectedText.replace(
           targetRegExp,
